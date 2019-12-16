1b5e6993dafde2fcdb49ed6465a659f444436fd1
PlusQi <614671436@qq.com>
2019-12-16 23:04:00 +0800

    商品分类

---
+++ eshop-item/eshop-item-service/src/main/java/com/eshop/item/controller/CategoryController.java
@@ -0,0 +1,29 @@
+package com.eshop.item.controller;
+
+import com.eshop.item.pojo.Category;
+import com.eshop.item.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /*
+    * 根据父节点的id查询子节点
+    * */
+    @GetMapping("list")
+    public ResponseEntity<List<Category>> queryCategoryListByPid(@RequestParam("pid")Long pid){
+        return ResponseEntity.ok(categoryService.queryCategoryListByPid(pid));
+    }
+}
