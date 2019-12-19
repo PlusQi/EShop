024b05b9adb2576ea46f0107d8f2f9605971731f
PlusQi <614671436@qq.com>
2019-12-19 23:07:04 +0800

    商品服务

---
+++ eshop-item/eshop-item-service/src/main/java/com/eshop/item/mapper/StockMapper.java
@@ -0,0 +1,10 @@
+package com.eshop.item.mapper;
+
+import com.eshop.common.mapper.BaseMapper;
+import com.eshop.item.pojo.Stock;
+import tk.mybatis.mapper.additional.idlist.IdListMapper;
+import tk.mybatis.mapper.additional.insert.InsertListMapper;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface StockMapper extends InsertListMapper<Stock> {
+}
