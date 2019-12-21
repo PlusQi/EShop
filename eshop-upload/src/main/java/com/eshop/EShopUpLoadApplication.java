75644f4b5ffe6553250a54fc584eb417e4f7d490
PlusQi <614671436@qq.com>
2019-12-21 21:36:58 +0800

    文件上传服务启动器

---
+++ eshop-upload/src/main/java/com/eshop/EShopUpLoadApplication.java
@@ -0,0 +1,13 @@
+package com.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EShopUpLoadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EShopUpLoadApplication.class);
+    }
+}
