e76365e9c1ceef3214f7fcc366b1ef8dd8ac5916
PlusQi <614671436@qq.com>
2019-12-26 23:20:41 +0800

    异常枚举

---
+++ eshop-common/src/main/java/com/eshop/common/enums/ExceptionEnum.java
@@ -0,0 +1,24 @@
+package com.eshop.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public enum ExceptionEnum {
+    BRAND_NOT_FOUND(404, "品牌不存在"),
+    CATEGORY_NOT_FOUND(404, "商品分类不存在"),
+    SPEC_GROUP_NOT_FOUND(404, "商品规格组不存在"),
+    SPEC_PARAM_NOT_FOUND(404, "商品规格参数不存在"),
+    GOODS_NOT_FOUND(404, "商品不存在"),
+    BRAND_SAVE_ERROR(500, "新增品牌失败"),
+    UPLOAD_FILE_ERROR(500, "文件上传失败"),
+    INVALID_FILE_TYPE(400, "无效的文件类型"),
+    GOODS_SAVE_ERROR(500, "新增商品失败"),
+    ;
+    private int code;
+    private String msg;
+
+}
