8f1f54d96f77339cc5579f869a9eccb9914ef79f
PlusQi <614671436@qq.com>
2019-12-28 20:36:51 +0800

    修改了一个通用Mapper继承

--- eshop-item/eshop-item-service/src/main/java/com/eshop/item/mapper/StockMapper.java
+++ eshop-item/eshop-item-service/src/main/java/com/eshop/item/mapper/StockMapper.java
@@ -3,8 +3,9 @@ package com.eshop.item.mapper;
 import com.eshop.common.mapper.BaseMapper;
 import com.eshop.item.pojo.Stock;
 import tk.mybatis.mapper.additional.idlist.IdListMapper;
+import tk.mybatis.mapper.additional.idlist.SelectByIdListMapper;
 import tk.mybatis.mapper.additional.insert.InsertListMapper;
 import tk.mybatis.mapper.common.Mapper;
 
-public interface StockMapper extends InsertListMapper<Stock> {
+public interface StockMapper extends BaseMapper<Stock> {
 }
